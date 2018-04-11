2df57264ffcac529331309f4464f2866595ad969
sunvsp <31379663+sunvsp@users.noreply.github.com>
2018-04-11 19:49:03 +0700

    สุ่ม private key

--- app/User.php
+++ app/User.php
@@ -15,6 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
+
         'username','name', 'email', 'password','private_key'
 
     ];
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,6 +25,11 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
+                            <label for="username" class="col-md-4 control-label">Username</label>
+
+                            <div class="col-md-6">
+                                <input id="username" type="text" class="form-control" name="username" value="{{ old('name') }}" required autofocus>
+
                                 @if ($errors->has('username'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('username') }}</strong>
