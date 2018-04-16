63ff3e5ac486de81e1622f95e7c38ae6ea54bfb9
stampspv <suphavich.c@gmail.com>
2018-04-16 14:34:56 +0700

    PRepare for Member Card
    PRepare for Member Card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,10 +14,59 @@
                         </div>
                     @endif
 
-                    You are logged in!
+<div class="row">
+    <div class="col-lg-5">
+        <div id="1" class="container_card" onClick="reply_click(this)">
+          <div class="card_home card1">
+            <div class="front" style="background: url(img/cards/test.png)top center;background-size: cover; z-index: 1">
+                Click to flip
+            </div>
+            <div class="back">
+                <img src="http://api.qrserver.com/v1/create-qr-code/?color=000000&amp;bgcolor=ffffff&amp;data=google" height="200" width="200">
+            </div>
+          </div>
+        </div>
+    </div>
+    <div class="col-lg-7">
+        <h3>Poseidon Member VIP Club</h3>
+    </div>
+</div>
+<hr>
+<div class="row">
+    <div class="col-lg-5">
+        <div id="2" class="container_card" onClick="reply_click(this)">
+          <div class="card_home card2">
+            <div class="front" style="background: url(img/cards/card2.png)top center;background-size: cover; z-index: 1">
+                Click to flip
+            </div>
+            <div class="back">
+                <img src="http://api.qrserver.com/v1/create-qr-code/?color=000000&amp;bgcolor=ffffff&amp;data=google" height="200" width="200">
+            </div>
+          </div>
+        </div>
+    </div>
+    <div class="col-lg-7">
+        <h3>Starbucks Reserve Member Card</h3>
+    </div>
+</div>
+<hr>
+
+
                 </div>
             </div>
         </div>
     </div>
 </div>
+@endsection
+
+
+@section('js')
+<script type="text/javascript">
+function reply_click(elem)
+{
+    var id = $(elem).attr("id");
+    var el = '.card'+id;
+    $(el).toggleClass('flipped');
+}
+</script>
 @endsection
