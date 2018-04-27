5d7466ab0776853c1787e4dcb4d261d5b24f0015
sunvsp <31379663+sunvsp@users.noreply.github.com>
2018-04-27 11:02:42 +0700

    setting view

--- resources/views/shops/setting.blade.php
+++ resources/views/shops/setting.blade.php
@@ -42,7 +42,7 @@
 
              
 
-              <div class="card" style="margin-left: -30px;height: 800px;">
+              <div class="card" style="margin-left: -30px;">
                 
                 <div class="card-body" style="padding:20px;">
                   <h1>&nbsp&nbsp&nbsp&nbspGeneral</h1>
@@ -108,6 +108,59 @@
                         </div>
                         </tr>
 
+                        <tr>
+                          <div class="form-group" style="margin-top:20px;">
+                          <label  class="col-md-3 control-label">Business Type</label>
+                            <div class="col-md-6" >
+                              <select name="type" class="form-control" required>
+                              <option value="beauty">Beauty</option>
+                              <option value="clothes">Clothes</option>
+                              <option value="drink">Drink</option>
+                              <option value="food">Food</option>
+                              <option value="jewellery">Jewellery</option>
+                              <option value="service">Service</option>
+                              </select>
+                            </div>
+                          </div>
+                        </tr>
+
+                        <tr>
+                        <div class="form-group" style="margin-top:20px;">
+                          <label  class="col-md-3 control-label">Business Hours</label>
+                            <div class="col-md-6">
+                            <div id="time">
+                                <div class="col-md-5">
+                                  <input id="timepicker1" width="100%"  name="timeOpen" required> 
+                                </div>
+                                <div class="col-md-1">
+                                  <span><b>-</b></span>
+                                </div>
+                                <div class="col-md-5">
+                                  <input id="timepicker2" width="100%"  name="timeClose" required >
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          </tr>
+
+                                    <!-- google map -->
+                          <tr>
+                          <div class="form-group" style="margin-top:30rpx;">
+                            <label  class="col-md-3 control-label">Shop Address</label>
+                            <div class="col-md-6">
+                                <input type="text" id="searchMap">
+                              <div id="mapCanvas"></div>
+                            </div>
+                          </div>
+                          </tr>
+
+                          <tr>
+                            <div class="form-group" style="margin-top:20px;">
+                            <div class="col-md-8 col-md-offset-3">
+                                <button id="myInput" type="submit" class="btn btn-primary" >&nbsp&nbspUpdate&nbsp&nbsp</button>
+                            </div>
+                            </div>
+                          </tr>
 
 
                         
@@ -132,3 +185,82 @@
 
 @endsection
 
+@section('js')
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBbR2fPnPAqOeegpN6ml_SNSMSc7wN613k&libraries=places&callback=initMap"
+    async defer></script>
+<script src="https://cdn.jsdelivr.net/npm/gijgo@1.9.6/js/gijgo.min.js" type="text/javascript"></script>
+<link href="https://cdn.jsdelivr.net/npm/gijgo@1.9.6/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+
+<script>
+    var map,geocoder,marker;
+
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('mapCanvas'), {
+          center: {lat: 13.7563, lng: 100.5018},
+          zoom: 15
+        });
+        marker = new google.maps.Marker( {
+          position: {lat: 13.7563, lng: 100.5018},
+          map: map,
+          draggable: true
+        });
+
+         var searchBox = new google.maps.places.SearchBox(document.getElementById('searchMap'));
+         google.maps.event.addListener(searchBox, 'places_changed' , function(){
+          var places = searchBox.getPlaces();
+          var bounds = new google.maps.LatLngBounds();
+          var i,place;
+
+          for (i=0; place=places[i];i++){
+              bounds.extend(place.geometry.location);
+              marker.setPosition(place.geometry.location);
+          }
+
+          map.fitBounds(bounds);
+          map.setZoom(15);
+      });
+      google.maps.event.addListener(marker, 'position_changed', function(){
+          var lat = marker.getPosition().lat();
+          var lng = marker.getPosition().lng();
+
+          $('#lat').val(lat);
+          $('#lng').val(lng);
+      });
+
+
+        
+      }
+
+    $('#timepicker1').timepicker({
+        uiLibrary: 'bootstrap'
+    });
+    $('#timepicker2').timepicker({
+        uiLibrary: 'bootstrap'
+    });
+
+    // $(document).keypress(function(e) {
+    // if(e.keyCode === 32 && e.target.nodeName=='BODY') {
+    //     event.preventDefault(); //prevent default if it is body
+    // }
+// });
+
+</script>
+
+@endsection
+
+@section('css')
+
+<style type="text/css">
+     #mapCanvas {
+        width: 450px;
+        height: 300px;
+      }
+      #searchMap{
+          width:450px;
+      }
+      .time , #timepicker1 , #timepicker2{
+    display: inline-block;
+}
+</style>
+
+@endsection
