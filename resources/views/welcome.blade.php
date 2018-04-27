6e34a575634b73c780455573256e7ed04574991a
chamamas <chamamas.k@ku.th>
2018-04-27 14:54:08 +0700

    welcomepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -164,7 +164,7 @@
             .border-bottom { border-bottom: 1px solid #e5e5e5; }
 
             .box-shadow { box-shadow: 0 .25rem .75rem rgba(0, 0, 0, .05);
-          
+
              }
 
             .flex-equal > * {
@@ -270,20 +270,7 @@
           </div>
         </div>
 
-        <div class="flex-center position-ref full-height">
-            <div class="content" >
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
+
         <div class="flex-center position-ref full-height">
           <div class="container">
             <div class="card-deck mb-3 text-center font-weight-bold">
@@ -335,27 +322,14 @@
             </div>
           </div>
         </div>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
+        
 
 
         <footer class="container py-5">
           <div class="row">
             <div class="col-12 col-md">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mb-2"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
-              <small class="d-block mb-3 text-muted">&copy; 2017-2018</small>
+              <small class="d-block mb-3 text-muted">&copy; 111</small>
             </div>
             <div class="col-6 col-md">
               <h5>Features</h5>
