94ee4cbbd2a5716c322910c303ec2e253862d7c4
sunvsp <31379663+sunvsp@users.noreply.github.com>
2018-04-30 15:33:49 +0700

    fix Bug shop

---
+++ resources/views/mail/forgetPassword.blade.php
@@ -0,0 +1,7 @@
+<p>We heard that you lost your BeCard password. Sorry about that!</p>
+
+<p>But don’t worry! You can use the following link to reset your password:</p>
+
+<p><a href="http://localhost:8000/resetPassword/{{$data->username}}">Reset Password Click here</a></p>
+
+<p>Thanks,Your friends at BeCard</p>
